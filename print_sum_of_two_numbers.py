bbbcd9f90200557b6e9a4aeb80419e41bec49b5a
aenonod <abegail.nonod13@gmail.com>
2025-03-10 22:47:10 +0800

    style: remove extra blank lines in print_sum_of_two_numbers.py

--- print_sum_of_two_numbers.py
+++ print_sum_of_two_numbers.py
@@ -1,8 +1,6 @@
 # Ask user to input 2 numbers
-
 num1 = int(input("Input first number: "))
 num2 = int(input("Input second number: "))
 
 # Print the sum of the two numbers
-
 print("The sum is:", num1 + num2)
