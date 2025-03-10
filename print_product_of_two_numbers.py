1dc086177c9c236aa34d49d80dc0236a00e17841
aenonod <abegail.nonod13@gmail.com>
2025-03-10 22:22:10 +0800

    add program code to print product of two numbers input by the user

--- print_product_of_two_numbers.py
+++ print_product_of_two_numbers.py
@@ -1,2 +1,8 @@
 # Ask user to input 2 numbers
+
+num1 = int(input("Input first number: "))
+num2 = int(input("Input second number: "))
+
 # Print the product of the two numbers
+
+print("The product is:", num1 * num2)
