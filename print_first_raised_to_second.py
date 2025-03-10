9bc6be418a6a3de688daa7ee4be97053f618b4ee
aenonod <abegail.nonod13@gmail.com>
2025-03-10 22:59:44 +0800

    add program code to print first num raised to second num input by the user

--- print_first_raised_to_second.py
+++ print_first_raised_to_second.py
@@ -1,2 +1,6 @@
 # Ask user to input 2 numbers
+num1 = int(input("Input first number: "))
+num2 = int(input("Input second number: "))
+
 # Print the result when the first number is raised to the second number
+print("The answer is:", num1 ** num2) 
