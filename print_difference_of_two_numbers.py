910664cff8162102fdc4ca120a140703a63398c2
aenonod <abegail.nonod13@gmail.com>
2025-03-11 00:55:41 +0800

    add program code to print difference of two numbers input by the user

--- print_difference_of_two_numbers.py
+++ print_difference_of_two_numbers.py
@@ -1,2 +1,6 @@
 # Ask user to input 2 numbers
+num1 = int(input("Input first number: "))
+num2 = int(input("Input second number: "))
+
 # Print the difference of the two numbers
+print("The difference is:", num1 - num2)
