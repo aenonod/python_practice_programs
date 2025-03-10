ad99cdc02b9c8fb23dd46e456bfeddcf2fd587c0
aenonod <abegail.nonod13@gmail.com>
2025-03-11 00:31:59 +0800

    docs: add pseudocode to print_smaller_number.py

---
+++ print_smaller_number.py
@@ -0,0 +1,3 @@
+# Ask user to input 2 numbers
+# If the first number is less than the second number, print the first number
+# If the second number is less than the first number, print the second number
