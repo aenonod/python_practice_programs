6eec688fa26979c13136f4fa887d539082adda29
aenonod <abegail.nonod13@gmail.com>
2025-03-11 01:14:52 +0800

    add program code to print remainder after division of numbers input by the user

--- print_remainder.py
+++ print_remainder.py
@@ -1,2 +1,7 @@
 # Ask user to input 2 numbers
+num1 = int(input("Input first number: "))
+num2 = int(input("Input second number: "))
+
 # Print the remainder when the first number is divided by the second number.
+remainder = num1 % num2
+print("The remainder is:", remainder)
