7a8deefe14899993c082ab28b55ca60ecf992e92
aenonod <abegail.nonod13@gmail.com>
2025-03-11 01:31:59 +0800

    add program code to print result of first number minus all remaining numbers

--- print_first_number_minus_all.py
+++ print_first_number_minus_all.py
@@ -1,3 +1,9 @@
 # Ask user to input 10 numbers
-# For loop (to shorten the code)
+given_numbers = []
+for i in range(10):     # For loop (to shorten the code)
+    num = int(input(f"Input number {i+1}: "))
+    given_numbers.append(num)
+
 # Print the result of the first number minus all of the remaining numbers
+result = given_numbers[0] - sum(given_numbers[1:])
+print("The result is:", result)
