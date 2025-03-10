d67e42859b197edd9f6f438c86738ea2a0556b72
aenonod <abegail.nonod13@gmail.com>
2025-03-10 23:44:12 +0800

    add program code to print odd among ten numbers input by the user

--- print_odd_input_by_user.py
+++ print_odd_input_by_user.py
@@ -1,3 +1,9 @@
 # Ask user to input 10 numbers
-# For loop (to shorten the code)
+odd = []
+for i in range(10):     # For loop (to shorten the code)
+    num = int(input(f"Input a number {i+1}: "))
+    if num % 2 != 0:
+        odd.append(num)
+
 # Print how many are odd numbers
+print(odd)
