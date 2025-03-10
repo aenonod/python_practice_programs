bb0c9e0df63083e82109a5b7a778c65af0bb4b08
aenonod <abegail.nonod13@gmail.com>
2025-03-11 00:37:23 +0800

    add program code to print the smaller number between two numbers input by the user

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -1,3 +1,11 @@
 # Ask user to input 2 numbers
+num1 = int(input("Input first number: "))
+num2 = int(input("Input second number: "))
+
 # If the first number is less than the second number, print the first number
+if num1 < num2:
+    print(f"{num1} is smaller than {num2}.")
+
 # If the second number is less than the first number, print the second number
+if num2 < num1:
+    print(f"{num2} is smaller than {num1}.")
