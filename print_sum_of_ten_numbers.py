4e41c9f4d882558e0d592081fb21aa26f0197f3e
aenonod <abegail.nonod13@gmail.com>
2025-03-10 23:25:20 +0800

    add program code to print sum of ten numbers input by the user

--- print_sum_of_ten_numbers.py
+++ print_sum_of_ten_numbers.py
@@ -1,3 +1,8 @@
 # Ask user to input 10 numbers
-# For loop (to shorten the code)
+sum = 0
+for i in range(10):     # For loop (to shorten the code)
+    num = int(input(f"Input a number {i+1}: "))
+    sum += num
+
 # Print the sum of all the numbers
+print("The sum is:", sum)
