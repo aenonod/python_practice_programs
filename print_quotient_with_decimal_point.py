0b841626782f48bc0d1eab63f96a8134dc5047ca
aenonod <abegail.nonod13@gmail.com>
2025-03-10 22:38:22 +0800

    add program code to print quotient of two numbers w decimal input by the user

--- print_quotient_with_decimal_point.py
+++ print_quotient_with_decimal_point.py
@@ -1,3 +1,15 @@
 # Ask user to input 2 numbers
+
+num1 = float(input("Input first number: "))
+num2 = float(input("Input second number: "))
+
 # Print the quotient of the two numbers with the decimal point
+
+if num2 != 0:
+    print("The quotient is:", num1 / num2)
+
 # State that division by zero is not allowed
+
+else:
+    print("Division by zero is not allowed.")
+    
