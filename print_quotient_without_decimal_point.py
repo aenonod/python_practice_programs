b561d3002b3b22a5dfd7dd2ef60749f991b7f1f1
aenonod <abegail.nonod13@gmail.com>
2025-03-11 01:02:12 +0800

    add program code to print quotient of two numbers w/o decimal input by the user

--- print_quotient_without_decimal_point.py
+++ print_quotient_without_decimal_point.py
@@ -1,3 +1,11 @@
 # Ask user to input 2 numbers
+num1 = int(input("Input first number: "))
+num2 = int(input("Input second number: "))
+
 # Print the quotient of the two numbers without the decimal point
+if num2 != 0:
+    print("The quotient is:", num1 // num2)
+
 # State that division by zero is not allowed
+else:
+    print("Division by zero is not allowed.")
