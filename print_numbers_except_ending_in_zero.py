e4bd23b186750aa1eebae4c35cc824ada0a4e215
aenonod <abegail.nonod13@gmail.com>
2025-03-11 00:06:59 +0800

    add program code to print numbers except ending in zero

--- print_numbers_except_ending_in_zero.py
+++ print_numbers_except_ending_in_zero.py
@@ -1,2 +1,7 @@
 # Print all the numbers starting from 0 to 100 except numbers ending in zero
-# For loop (to shorten the code)
+exc_zero = []
+for i in range(0, 101):     # For loop (to shorten the code)
+    if i % 10 != 0:
+        exc_zero.append(i)
+
+print(exc_zero)
