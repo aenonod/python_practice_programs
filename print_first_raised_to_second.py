50f98befef4430bcce9a9073793e2d049d15c9e2
aenonod <abegail.nonod13@gmail.com>
2025-03-10 23:06:09 +0800

    feat: enhance output with clearer labels

--- print_first_raised_to_second.py
+++ print_first_raised_to_second.py
@@ -3,4 +3,4 @@ num1 = int(input("Input first number: "))
 num2 = int(input("Input second number: "))
 
 # Print the result when the first number is raised to the second number
-print("The answer is:", num1 ** num2) 
+print(f"{num1} raised to {num2} is:", num1 ** num2) 
