e9d7e31e24d1ca110d180c5559008835e5fc4615
aenonod <abegail.nonod13@gmail.com>
2025-03-11 23:45:29 +0800

    add program code to print numbers without duplicate

--- print_numbers_without_duplicate.py
+++ print_numbers_without_duplicate.py
@@ -1,2 +1,9 @@
 # Ask user to input 10 numbers
+given_numbers = []
+for i in range(10):     # For loop (to shorten the code)
+    num = int(input(f"Input number {i+1}: "))
+    given_numbers.append(num)
+
 # Display all numbers that don't have duplicate
+unique_numbers = [num for num in given_numbers if given_numbers.count(num) == 1]
+print("Numbers without duplicate:", unique_numbers)
