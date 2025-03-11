e5a81ff7ce87afc8b418c1eac99ddbff8072d8f9
aenonod <abegail.nonod13@gmail.com>
2025-03-12 00:25:22 +0800

    docs: add pseudocode to print_all_with_first_entry_if_duplicate.py

---
+++ print_all_with_first_entry_if_duplicate.py
@@ -0,0 +1,4 @@
+# Ask user to input 10 numbers
+# Use class set for easier tracking of numbers that have already been displayed
+# Display all numbers
+# For numbers with duplicate, display only the first entry
