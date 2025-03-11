123b03f387c8c7eb868afe90cb99ff4e895aeba8
aenonod <abegail.nonod13@gmail.com>
2025-03-11 18:31:35 +0800

    add program code to print odd starting from 0 to 100

--- print_odd_zero_to_one_hundred.py
+++ print_odd_zero_to_one_hundred.py
@@ -1,2 +1,5 @@
 # Print all the odd numbers starting from 0 to 100
-# While loop (as per instructions)
+num = 1
+while num <= 100:     # While loop (as per instructions)
+    print(num)
+    num += 2
