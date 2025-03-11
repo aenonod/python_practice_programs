a2f2912b4608b4c3f7c5eb71d27dfe862dc9ad30
aenonod <abegail.nonod13@gmail.com>
2025-03-11 18:16:58 +0800

    add program code to print even among ten numbers input by the user

--- print_even_input_by_user.py
+++ print_even_input_by_user.py
@@ -1,3 +1,9 @@
 # Ask user to input 10 numbers
-# For loop (to shorten the code)
+even = []
+for i in range(10):     # For loop (to shorten the code)
+    num = int(input(f"Input number {i+1}: "))
+    if num % 2 == 0:
+        even.append(num)
+
 # Print how many are even numbers
+print(even)
