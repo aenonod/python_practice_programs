b7fedbc471d92492a8735e1ab9182a0da7b6ab34
aenonod <abegail.nonod13@gmail.com>
2025-03-12 00:39:12 +0800

    add program code to print all numbers with first entry of duplicate

--- print_all_with_first_entry_if_duplicate.py
+++ print_all_with_first_entry_if_duplicate.py
@@ -1,4 +1,15 @@
 # Ask user to input 10 numbers
+given_numbers = []
+for i in range(10):
+    num = int(input(f"Input number {i+1}: "))
+    given_numbers.append(num)
+
 # Use class set for easier tracking of numbers that have already been displayed
+seen = set()
+
 # Display all numbers
 # For numbers with duplicate, display only the first entry
+for num in given_numbers:
+    if num not in seen:
+        print(num, end=', ')
+        seen.add(num)
