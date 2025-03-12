7a6565d04d8269b9d66f9d901d35d4369b964417
aenonod <abegail.nonod13@gmail.com>
2025-03-12 15:56:52 +0800

    add program code to print lowest to highest numbers

--- print_lowest_to_highest.py
+++ print_lowest_to_highest.py
@@ -1,3 +1,15 @@
 # Ask user to input a number, continue asking until the user input is invalid
-# While loop (keep asking the user for input until invalid input is entered)
+given_numbers = []
+while True:     # While loop (keep asking the user for input until invalid input is entered)
+    user_input = input("Input a number: ")
+
+    if not user_input.isdigit():
+        print("Invalid input. Exiting...")
+        break
+
+    number = int(user_input)
+    given_numbers.append(number) 
+
 # Display the number from lowest to highest using sort() function
+    given_numbers.sort()
+    print(given_numbers)
