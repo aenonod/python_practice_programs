7165d10f1804aeedbab7672aa37968e60a68e4be
aenonod <abegail.nonod13@gmail.com>
2025-03-12 22:29:31 +0800

    add program code to print highest number

--- print_highest_number.py
+++ print_highest_number.py
@@ -1,3 +1,17 @@
 # Ask user to input a number, continue asking until the user input is invalid
-# Use while loop since input is not limited
-# Display the highest number
+highest_number = None
+while True:     # Use while loop since input is not limited
+    user_input = input("Input a number: ")
+
+    if not user_input.isdigit():
+        print("Invalid input. Exiting...")
+        break
+
+    number = int(user_input)
+
+# Display the highest 
+    if highest_number is None or number > highest_number:
+        highest_number = number
+        print("Highest:", highest_number)
+    else:
+        print("Highest:", highest_number)
