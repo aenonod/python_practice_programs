a82e30c80e9317c047c6477a9d9f204d823f1719
aenonod <abegail.nonod13@gmail.com>
2025-03-12 13:31:25 +0800

    add program code to print unique or duplicate

--- print_unique_or_duplicate.py
+++ print_unique_or_duplicate.py
@@ -1,4 +1,18 @@
 # Ask user to input a number, continue asking until the user input is invalid
-# While loop (keep asking the user for input until invalid input is entered)
+given_numbers = []
+while True:     # While loop (keep asking the user for input until invalid input is entered)
+    num = input("Input a number: ")
+
+# Check if the user input is a digit
+    if not num.isdigit():
+        print("Invalid input. Exiting...")
+        break
+
 # Display "Unique" after input when the inputted number don't have duplicate
+    if num not in given_numbers:
+        print("Unique")
+        given_numbers.append(num)
+
 # Display "Duplicate" after input when the inputted number have duplicate
+    else:
+        print("Duplicate")
