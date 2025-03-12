7b8f1738db939ac261b9a746aef3c4bf01a0b555
aenonod <abegail.nonod13@gmail.com>
2025-03-12 23:29:33 +0800

    refactor: replace list comprehension with code block

--- print_numbers_without_duplicate.py
+++ print_numbers_without_duplicate.py
@@ -5,5 +5,9 @@ for i in range(10):     # For loop (to shorten the code)
     given_numbers.append(num)
 
 # Display all numbers that don't have duplicate
-unique_numbers = [num for num in given_numbers if given_numbers.count(num) == 1]
+unique_numbers = []
+for num in given_numbers:
+    if given_numbers.count(num) == 1:
+        unique_numbers.append(num)
+
 print("Numbers without duplicate:", unique_numbers)
