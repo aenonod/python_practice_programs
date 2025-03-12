4ac9ef4afd70ba1b1da5cd84b514c92c0c6edfd2
aenonod <abegail.nonod13@gmail.com>
2025-03-12 18:05:10 +0800

    add program code to print numbers with duplicate

--- print_numbers_with_duplicate.py
+++ print_numbers_with_duplicate.py
@@ -1,3 +1,17 @@
 # Ask user to input 10 numbers
-# For loop (to shorten the code)
+given_numbers = []
+for i in range(10):     # For loop (to shorten the code)
+    num = int(input(f"Input number {i+1}: "))
+    given_numbers.append(num)
+
+# Use class set for easier tracking of numbers that have already been displayed
+seen = set()
+with_duplicate = []
+
 # Display all numbers that have duplicate
+for num in given_numbers:
+    if given_numbers.count(num) != 1 and num not in seen:
+        with_duplicate.append(num)
+        seen.add(num)
+
+print("Numbers with duplicate:", with_duplicate)
