7b7999ac7f016690c4bd1e9a1a24b45c9b6319f2
aenonod <abegail.nonod13@gmail.com>
2025-03-12 13:07:43 +0800

    docs: add pseudocode to print_unique_or_duplicate.py

---
+++ print_unique_or_duplicate.py
@@ -0,0 +1,4 @@
+# Ask user to input a number, continue asking until the user input is invalid
+# While loop (keep asking the user for input until invalid input is entered)
+# Display "Unique" after input when the inputted number don't have duplicate
+# Display "Duplicate" after input when the inputted number have duplicate
