4ebfe3fa5992ffeed9d81ed1d13fb0d6f0142998
aenonod <abegail.nonod13@gmail.com>
2025-03-12 13:53:48 +0800

    docs: add pseudocode to print_lowest_number.py

---
+++ print_lowest_number.py
@@ -0,0 +1,3 @@
+# Ask user to input a number, continue asking until the user input is invalid
+# While loop (keep asking the user for input until invalid input is entered)
+# Display the lowest number
