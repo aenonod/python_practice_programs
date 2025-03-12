ec13e366f6aeb73c238def0c17b837a6f44a04d5
aenonod <abegail.nonod13@gmail.com>
2025-03-12 21:46:58 +0800

    add while loop and code to count occurrences of each number

--- print_most_number_of_duplicate.py
+++ print_most_number_of_duplicate.py
@@ -1,6 +1,22 @@
 # Ask user to input a number, continue asking until the user input is invalid
-# Use while loop since input is not limited
+given_numbers = []
+while True:     # Use while loop since input is not limited
+    try:
+        num = int(input("Input a number: "))
+        given_numbers.append(num)
+
 # Count occurrences of each number
+        number_counts = {}
+        for num in given_numbers:
+            if num in number_counts:
+                number_counts[num] += 1
+            else:
+                number_counts[num] = 1
+
 # Find the maximum count (most frequent number)
 # Find all numbers with the maximum count
 # Display the numbers with the most duplicates
+
+    except ValueError:
+        print("Invalid input. Exiting...")
+        break
