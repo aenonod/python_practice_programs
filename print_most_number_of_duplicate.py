69d6772edaa0107016b1253d9ab24f8bbbcd9dfa
aenonod <abegail.nonod13@gmail.com>
2025-03-12 19:28:01 +0800

    docs: add pseudocode to print_most_number_of_duplicate.py

---
+++ print_most_number_of_duplicate.py
@@ -0,0 +1,4 @@
+# Aask user to input a number, continue asking until the user input is invalid
+# While loop (keep asking the user for input until invalid input is entered)
+# Track duplicates
+# Display the number with the most number of duplicate
