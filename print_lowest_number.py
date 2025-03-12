90e97cb399b4003c9f4a14a24e2ac7dc46cbed52
aenonod <abegail.nonod13@gmail.com>
2025-03-12 14:19:19 +0800

    add program code to print lowest number

--- print_lowest_number.py
+++ print_lowest_number.py
@@ -1,3 +1,17 @@
 # Ask user to input a number, continue asking until the user input is invalid
-# While loop (keep asking the user for input until invalid input is entered)
+lowest_number = None
+while True:     # While loop (keep asking the user for input until invalid input is entered)
+    user_input = input("Input a number: ")
+
+    if not user_input.isdigit():
+        print("Invalid input. Exiting...")
+        break
+
+    number = int(user_input)
+
 # Display the lowest number
+    if lowest_number is None or number < lowest_number:
+        lowest_number = number
+        print("Lowest:", lowest_number)
+    else:
+        print("Lowest:", lowest_number)
